d19aea941843ba39c19a90d6bcae488e122dc2e2
asaevich <semen.asaevich@gmail.com>
2021-02-14 23:26:48 +0300

    create container for notes

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let textarea = document.querySelector('textarea')
+let dateInput = document.querySelector('input[type="date"]')
+
+let list = document.createElement('div')
+list.classList.add('list')
+document.body.appendChild(list)
