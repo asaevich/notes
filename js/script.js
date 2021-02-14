dcdb260c1a8db38321bda64156e85e8c0f29449c
asaevich <semen.asaevich@gmail.com>
2021-02-14 23:27:14 +0300

    create db

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ let dateInput = document.querySelector('input[type="date"]')
 let list = document.createElement('div')
 list.classList.add('list')
 document.body.appendChild(list)
+
+let db;
+// IIFE
+(async () => {
+    // создаем базу данных
+    // название, версия...
+    db = await idb.openDb('db', 1, db => {
+        // создаем хранилище
+        db.createObjectStore('notes', {
+            keyPath: 'id'
+        })
+    })
+
+    // формируем список
+    createList()
+})();
